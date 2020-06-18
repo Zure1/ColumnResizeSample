a2879261b673ffb90ed9c1b64b526b319b081e91
Schröer, Dennis <d.schroeer@optadata.de>
2020-06-18 12:21:46 +0200

    initial commit

---
+++ ColumnResizeSample/ColumnResizeSample/ColumnResizeSample/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ColumnResizeSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private double _colSize = 0.0;
+        private List<ColumnDefinition> _columns = new List<ColumnDefinition>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            MessagingCenter.Subscribe<MainPageViewModel>(this, "ResetColSize", (sender) =>
+            {
+                _colSize = 0.0;
+            });
+        }
+
+        private void Label_Value_SizeChanged(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var label = (Label)sender;
+                var grid = (Grid)label.Parent;
+                var column = grid.ColumnDefinitions[0];
+                if (!_columns.Contains(column))
+                {
+                    _columns.Add(column);
+                }
+                var adjustments = new List<ColumnDefinition>();
+                if (label.Width > _colSize)
+                {
+                    _colSize = label.Width;
+                    adjustments.AddRange(_columns);
+                }
+                else
+                {
+                    adjustments.Add(column);
+                }
+                foreach (var col in adjustments)
+                {
+                    col.Width = new GridLength(_colSize);
+                }
+            });
+        }
+    }
+}
---
+++ ColumnResizeSample/ColumnResizeSample/ColumnResizeSample/MainPageViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ColumnResizeSample
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _isLongData;
+        public ICommand ChangeDataCommand { get; set; }
+
+        private List<string> _shortDataList;
+        private List<string> _longDataList;
+
+        private ObservableCollection<string> _myList;
+        public ObservableCollection<string> MyList
+        {
+            get { return _myList; }
+            set
+            {
+                if (_myList != value)
+                {
+                    _myList = value;
+
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MyList"));
+                }
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            _shortDataList = new List<string>()
+            {
+                "abcee",
+                "abc",
+                "abc",
+                "abc",
+                "abc",
+                "abcg",
+                "abc",
+                "abc",
+                "abc",
+                "abc",
+            };
+
+            _longDataList = new List<string>()
+            {
+                "long text 123",
+                "long text 1231231123123",
+                "long text 123",
+                "long text 123",
+                "long text 123",
+                "long text 123",
+                "long t 123",
+                "long text 123",
+                "long text 1231111",
+                "long text 123"
+            };
+
+            ChangeDataCommand = new Command(
+                execute: () =>
+                {
+                    MessagingCenter.Send(this, "ResetColSize");
+
+                    if (_isLongData)
+                    {
+                        MyList = new ObservableCollection<string>(_shortDataList);
+                        _isLongData = false;
+                    }
+                    else
+                    {
+                        MyList = new ObservableCollection<string>(_longDataList);
+                        _isLongData = true;
+                    }
+                });
+
+            MyList = new ObservableCollection<string>(_shortDataList);
+        }
+    }
+}
